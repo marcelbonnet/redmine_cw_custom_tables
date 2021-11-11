46d9f09ebc7f87757cda8b5a0f4f9221c21b4c98
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2021-11-11 19:51:08 -0300

    WIP: add/destroy table permission

--- app/controllers/table_memberships_controller.rb
+++ app/controllers/table_memberships_controller.rb
@@ -2,7 +2,26 @@ class TableMembershipsController < ApplicationController
   layout 'admin'
   self.main_menu = false
 
+  before_action :require_admin
+  before_action :find_group, :only => [:new, :create]
   before_action :find_membership, :only => [:edit, :update, :destroy]
+
+  def new
+    @tables = CustomTable.active.all
+    @roles = Role.find_all_givable
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def create
+    @members = TableMember.create_table_memberships(@group, params[:membership])
+    respond_to do |format|
+      format.html {redirect_to_tab @group}
+      format.js
+    end
+  end
 
   def edit
     @roles = Role.givable.to_a
@@ -16,7 +35,7 @@ class TableMembershipsController < ApplicationController
     @membership.attributes = params.require(:membership).permit(:role_ids => [])
     @membership.save
     respond_to do |format|
-      format.html {redirect_to_tab }
+      format.html {redirect_to_tab @group}
       format.js
     end
   end
@@ -26,7 +45,7 @@ class TableMembershipsController < ApplicationController
       @membership.destroy
     # end
     respond_to do |format|
-      format.html {redirect_to_tab}
+      format.html {redirect_to_tab @group}
       format.js
     end
   end
@@ -43,8 +62,14 @@ class TableMembershipsController < ApplicationController
     end
   end
 
-  def redirect_to_tab
-    redirect_to edit_polymorphic_path(@group, :tab => 'table_memberships')
+  def find_group
+    @group = Group.find(params[:group_id])
+    rescue ActiveRecord::RecordNotFound
+      render_404
+  end
+
+  def redirect_to_tab(group)
+    redirect_to edit_polymorphic_path(group, :tab => 'table_memberships')
   end
 
 end
